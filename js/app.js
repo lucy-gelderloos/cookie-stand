7bb15cddf64ddfde394f463ae615df6ecf2c0846
Lucy Gelderloos <lucygel@gmail.com>
2022-06-13 19:35:37 -0700

    Add first object

--- js/app.js
+++ js/app.js
@@ -6,13 +6,19 @@ const seattle = {
   minCust: 23,
   maxCust: 65,
   avgSale: 6.3,
-  randCust: function(){
-    (Math.floor(Math.random() * (this.maxCust - this.minCust))) + 1;
-  },
-  hourlySales: function(){
-    let salesPerHour = [];
-    for (let i = 0; i < (this.openHour - this.closeHour); i++);
-    salesPerHour.push(this.randCust * this.avgSale);
-    console.log salesPerHour;
+  salesModel(){
+    let hourlySales = [];
+    for (let i = 0; i < (this.closeHour - this.openHour); i++){
+      let randCust = ((Math.floor(Math.random() * (this.maxCust - this.minCust))) + this.minCust + 1);
+      hourlySales.push(Math.round(randCust * this.avgSale));
+    }
+    console.log(hourlySales);
+    let totalSales = 0;
+    for (let i = 0; i < hourlySales.length; i++){
+      totalSales += hourlySales[i];
+    }
+    console.log(`Total Sales: ${totalSales}`);
   },
 };
+
+seattle.salesModel();
