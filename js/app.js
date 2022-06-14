c1968cff3388ea20e586bb59870bf00ee7d1d4f5
Lucy Gelderloos <lucygel@gmail.com>
2022-06-14 16:37:21 -0700

    Add first workng constructor

--- js/app.js
+++ js/app.js
@@ -1,5 +1,81 @@
 'use strict';
 
+const modelPrototype = {
+  openHour:6,
+  closeHour:20,
+};
+
+
+
+function Model(location,minCust,maxCust,avgSale){
+  this.storeLocation = location;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgSale = avgSale;
+  this.salesModel = function() {
+    let hourlySales = [];
+    let hour = [];
+    let salesByHour = [];
+    //model hourly sales
+    for (let i = this.openHour; i < this.closeHour; i++){
+      //generate random sales
+      let randCust = ((Math.floor(Math.random() * (this.maxCust - this.minCust))) + this.minCust + 1);
+      if(i < 12){
+        hour = (`${i}:00 am`);
+      }
+      else if(i === 12){
+        hour = (`${i}:00 pm`);
+      }
+      else{
+        hour = (`${(i-12)}:00 pm`);
+      }
+      hourlySales.push(Math.round(randCust * this.avgSale));
+      salesByHour.push(`${hour}: ${Math.round(randCust * this.avgSale)} cookies`);
+
+    }
+    console.log(hourlySales);
+    console.log(hour);
+    //calculate total sales
+    let totalSales = 0;
+    for (let j = 0; j < hourlySales.length; j++){
+      totalSales += hourlySales[j];
+    }
+    console.log(`Total Sales: ${totalSales}`);
+    salesByHour.push(`Total Daily Sales: ${totalSales}`);
+    //generate heading
+    const modelDiv = document.createElement('div');
+    modelDiv.id = this.storeLocation;
+    modelDiv.className = ('model-div');
+    const locationHeading = document.createElement('h2');
+    const locationHeadingContent = document.createTextNode(this.storeLocation);
+    locationHeading.appendChild(locationHeadingContent);
+    modelDiv.appendChild(locationHeading);
+
+    const locationModelList = document.createElement('ul');
+    locationModelList.id = `${this.storeLocation}-list`;
+    locationModelList.className = ('model-list');
+    salesByHour.forEach((item)=>{
+      let li = document.createElement('li');
+      li.innerText = item;
+      locationModelList.appendChild(li);
+    }
+    );
+    modelDiv.appendChild(locationModelList);
+    const modelSection = document.getElementById('Insert').parentNode;
+    document.body.insertBefore(modelDiv, modelSection);
+
+
+  };
+}
+
+Model.prototype = modelPrototype;
+Model.prototype.constructor = Model;
+
+
+const portland = new Model('Portland',18,45,4);
+console.log (portland);
+
+
 const seattle = {
   storeLocation: 'Seattle',
   openHour: 6,
@@ -40,6 +116,7 @@ const seattle = {
     //generate heading
     const modelDiv = document.createElement('div');
     modelDiv.id = this.storeLocation;
+    modelDiv.className = ('model-div');
     const locationHeading = document.createElement('h2');
     const locationHeadingContent = document.createTextNode(this.storeLocation);
     locationHeading.appendChild(locationHeadingContent);
@@ -47,6 +124,7 @@ const seattle = {
 
     const locationModelList = document.createElement('ul');
     locationModelList.id = `${this.storeLocation}-list`;
+    locationModelList.className = ('model-list');
     salesByHour.forEach((item)=>{
       let li = document.createElement('li');
       li.innerText = item;
