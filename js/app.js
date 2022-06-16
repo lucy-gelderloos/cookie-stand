d5120d16048faa40bfb8b2d3c36e946dd1edaee5
Lucy Gelderloos <lucygel@gmail.com>
2022-06-15 20:59:52 -0700

    Add showSlides

--- js/app.js
+++ js/app.js
@@ -210,3 +210,22 @@ function stickyNav() {
     content.classList.remove('sticky-padding');
   }
 }
+
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_auto
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName('mySlides');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex-1].style.display = 'block';
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
