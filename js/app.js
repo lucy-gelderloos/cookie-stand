4781795bc0f32b64740e3bb1060e1c7210070aef
Lucy Gelderloos <lucygel@gmail.com>
2022-06-18 13:08:18 -0700

    Starting over, more or less

--- js/app.js
+++ js/app.js
@@ -1,262 +1,19 @@
 'use strict';
 
-function openHours() {
-  let hoursHeader = ['Location'];
-  let hours = [];
-  for (let i = 6; i < 20; i++){
-    if(i < 12){
-      hours = (`${i}:00 am`);
-    }
-    else if(i === 12){
-      hours = (`${i}:00 pm`);
-    }
-    else{
-      hours = (`${(i-12)}:00 pm`);
-    }
-    hoursHeader.push(hours);
-  }
-  hoursHeader.push('Daily Total Sales');
-  // console.log(hoursHeader);
+const salesCurve = [0.5, 0.75, 1.0, 0.6, 0.8, 1.0, 0.7, 0.4, 0.6, 0.9, 0.7, 0.5, 0.3, 0.4, 0.6];
 
-  const modelTable = document.getElementById('modelTable');
-  const headRow = document.createElement('tr');
-  // headRow.id = ('headerRow');
-  modelTable.appendChild(headRow);
-
-  for(let i = 0; i < hoursHeader.length; i++){
-    const colHead = document.createElement('th');
-    colHead.appendChild(document.createTextNode(hoursHeader[i]));
-    headRow.appendChild(colHead);
-  }
+function hoursFormatted() {
+  // this will create an array of the hours each store is open, including colon, am/pm
+  // remember to use mod for calculating am/pm
+  // it's not a prototype because the formatted hours are the same data as the hours it's open (and also because I'll probably just write this as if all the hours are the same)
 }
 
-openHours();
 
-function staffHours() {
-  let staffHoursHeader = ['Location'];
-  let staffHours = [];
-  for (let i = 6; i < 20; i++){
-    if(i < 12){
-      staffHours = (`${i}:00 am`);
-    }
-    else if(i === 12){
-      staffHours = (`${i}:00 pm`);
-    }
-    else{
-      staffHours = (`${(i-12)}:00 pm`);
-    }
-    staffHoursHeader.push(staffHours);
-  }
-
-  // console.log(staffHoursHeader);
-
-  const staffTable = document.getElementById('staffTable');
-  const staffHeadRow = document.createElement('tr');
-  staffTable.appendChild(staffHeadRow);
-
-  for(let i = 0; i < staffHoursHeader.length; i++){
-    const staffColHead = document.createElement('th');
-    staffColHead.appendChild(document.createTextNode(staffHoursHeader[i]));
-    staffHeadRow.appendChild(staffColHead);
-  }
-}
-
-staffHours();
-
-Model.prototype.salesModel = function(){
-  let hourlySales = [];
-  let scaleSales = [0.5, 0.75, 1.0, 0.6, 0.8, 1.0, 0.7, 0.4, 0.6, 0.9, 0.7, 0.5, 0.3, 0.4, 0.6];
-  //model hourly sales
-  for (let i = 6; i < 20; i++){
-    let randCust = ((Math.floor(Math.random() * (this.maxCust - this.minCust))) + this.minCust + 1);
-    hourlySales.push(Math.round(randCust * (this.avgSale * scaleSales[i-6])));
-  }
-  return(hourlySales);
-};
-
-Model.prototype.totalDailySales = function(){
-  let totalSales = 0;
-  for (let i = 0; i < this.hourlySales.length; i++){
-    totalSales += this.hourlySales[i];
-  }
-  return(totalSales);
-};
-
-Model.prototype.render = function(){
-  // const modelTable = document.getElementById('modelTable');
-  const modelLastRow = document.getElementById('salesFooter');
-
-  const salesRow = document.createElement('tr');
-  modelLastRow.parentNode.insertBefore(salesRow,modelLastRow);
-
-  const salesRowHead = document.createElement('td');
-  salesRowHead.className = ('left');
-  salesRowHead.appendChild(document.createTextNode(this.storeLocation));
-  salesRow.appendChild(salesRowHead);
-
-  for (let i = 0; i < 14; i++){
-    const cell = document.createElement('td');
-    cell.appendChild(document.createTextNode(this.hourlySales[i]));
-    salesRow.appendChild(cell);
-  }
-  const totalCell = document.createElement('td');
-  totalCell.appendChild(document.createTextNode(this.totalDailySales));
-  salesRow.appendChild(totalCell);
-
-  // const staffTable = document.getElementById('staffTable');
-  const staffLastRow = document.getElementById('staffFooter');
-
-  const staffRow = document.createElement('tr');
-  staffLastRow.parentNode.insertBefore(staffRow,staffLastRow);
-
-  const staffRowHead = document.createElement('td');
-  staffRowHead.className = ('left');
-  staffRowHead.appendChild(document.createTextNode(this.storeLocation));
-  staffRow.appendChild(staffRowHead);
-
-  for (let i = 0; i < 14; i++){
-    const cell = document.createElement('td');
-    cell.appendChild(document.createTextNode(this.hourlyStaff[i]));
-    staffRow.appendChild(cell);
-  }
+Model.prototype.addressRender = function() {
 
 };
 
-Model.prototype.hourlyStaffing = function(){
-  let hourlyStaff = [];
-  const minStaff = 2;
-  const cookiesPerStaff = 20;
-  for (let i = 0; i < this.hourlySales.length; i++){
-    let staffNeeded = Math.ceil(this.hourlySales[i]/cookiesPerStaff);
-    if(staffNeeded < minStaff){
-      hourlyStaff.push(minStaff);
-    }
-    else{
-      hourlyStaff.push(staffNeeded);
-    }
-  }
-  return(hourlyStaff);
-};
 
-const seattleArray = ['Seattle',23,65,6.3];
-const tokyoArray = ['Tokyo',3,24,1.2];
-const dubaiArray = ['Dubai',11,38,3,7];
-const parisArray = ['Paris',20,38,2.3];
-const limaArray = ['Lima',2,16,4.6];
-
-const locationsArray = ['seattle','tokyo','dubai','paris','lima'];
-
-const seattle = new Model(...seattleArray);
-const tokyo = new Model(...tokyoArray);
-const dubai = new Model(...dubaiArray);
-const paris = new Model(...parisArray);
-const lima = new Model(...limaArray);
-
-let hourlyTotalsArray = [];
-
-function hourlyTotals(){
-
-  // let hourlyTotalsArray = [];
-  hourlyTotalsArray.push('Total Sales');
-  for(let i = 0; i < 14; i++){
-    let hourTotal = (seattle.hourlySales[i] + tokyo.hourlySales[i] + dubai.hourlySales[i] + paris.hourlySales[i] + lima.hourlySales[i]);
-    hourlyTotalsArray.push(hourTotal);
-  }
-
-  let grandTotal = seattle.totalDailySales + tokyo.totalDailySales + dubai.totalDailySales + paris.totalDailySales + lima.totalDailySales;
-  hourlyTotalsArray.push(grandTotal);
-
-  // console.log (hourlyTotalsArray);
-
-  const modelTable = document.getElementById('modelTable');
-  const footerRow = document.createElement('tr');
-  footerRow.id = ('salesFooter');
-  modelTable.appendChild(footerRow);
-
-  for(let i = 0; i < hourlyTotalsArray.length; i++){
-    const hourlyTotalCell = document.createElement('th');
-    hourlyTotalCell.appendChild(document.createTextNode(hourlyTotalsArray[i]));
-    footerRow.appendChild(hourlyTotalCell);
-  }
-  return(hourlyTotalsArray);
-}
-
-hourlyTotals();
-
-// console.log(hourlyTotalsArray);
-
-function staffTotals(){
-  let staffTotalsArray = [];
-  staffTotalsArray.push('Total Staff');
-  for(let i = 0; i < 14; i++){
-    let staffTotal = (seattle.hourlyStaff[i] + tokyo.hourlyStaff[i] + dubai.hourlyStaff[i] + paris.hourlyStaff[i] + lima.hourlyStaff[i]);
-    staffTotalsArray.push(staffTotal);
-  }
-
-  const staffTable = document.getElementById('staffTable');
-  const staffFooterRow = document.createElement('tr');
-  staffFooterRow.id = ('staffFooter');
-  staffTable.appendChild(staffFooterRow);
-
-  for(let i = 0; i < staffTotalsArray.length; i++){
-    const staffTotalCell = document.createElement('th');
-    staffTotalCell.appendChild(document.createTextNode(staffTotalsArray[i]));
-    staffFooterRow.appendChild(staffTotalCell);
-  }
-}
-
-staffTotals();
-
-//form start
-
-//what needs to happen on click:
-// x new row created with location name and est. hourly sales/staffing
-// x row inserted above total row
-// - hourly sales/staffing from new store added to existing totals
-
-
-let formElement = document.getElementById('add-location');
-// let newStore;
-
-formElement.addEventListener('submit', function(event) {
-  event.preventDefault(); // prevent the refresh on submit
-
-  let newStoreLocation = event.target.locationName.value;
-  let newMinCust = Number(event.target.minCust.value);
-  // If these aren't converted to numbers, the math is bad
-  let newMaxCust = Number(event.target.maxCust.value);
-  let newAvgSale = Number(event.target.avgSale.value);
-
-  // newStoreArray.push(newStoreLocation,newMinCust,newMaxCust,newAvgSale);
-
-  // let newStoreArray = [newStoreLocation, newMinCust, newMaxCust, newAvgSale];
-  locationsArray.push(newStoreLocation.toLowerCase()); //this works, in case I ever decide what to do with it
-  let newStore = new Model(newStoreLocation,newMinCust,newMaxCust,newAvgSale);
-  // console.log(newStore.hourlySales);
-  newStore.render();
-
-  let oldTotalSales = document.getElementById('salesFooter');
-  oldTotalSales.remove;
-  // let hourlyTotalsArray = hourlyTotalsArray;
-  // for(let i = 0; i < 14; i++){
-  //   let hourTotal = (hourlyTotalsArray[i] + newStoreLocation.hourlySales[i]);
-  //   hourlyTotalsArray.push(hourTotal);
-  // }
-
-  console.log(hourlyTotalsArray);
-
-});
-
-//form end
-
-//Spread syntax (...) - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
-
-seattle.render();
-tokyo.render();
-dubai.render();
-paris.render();
-lima.render();
-// portland.render();
 
 
 function Model(location,minCust,maxCust,avgSale){
@@ -264,10 +21,34 @@ function Model(location,minCust,maxCust,avgSale){
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgSale = avgSale;
-  this.hourlySales = this.salesModel();
-  this.totalDailySales = this.totalDailySales();
-  this.hourlyStaff = this.hourlyStaffing();
 }
+
+
+/*
+
+Each store has:
+ - city/location (add address, contact details)
+ - minimum estimated customers/hour
+ - maxiumum estimated customers/hour
+ - average cookies/sale
+ - opening time (same for all stores)
+ - closing time (same for all stores)
+ x sales curve (same for all stores)
+
+Each store needs:
+ - how many hours is it open? (same for all stores)
+ - how many cookies is it estimated to sell per hour?
+ - how many cookies is it estimated to sell per day?
+ - how many staff does it need per hour? (assuming each staff member can sell 20 cookies/hour and each store needs a minimum of two staff per hour)
+ - contact info on the
+
+Each TABLE needs:
+ - header row of hours (ideally with the flexibility to accommodate different hours, but not required)
+ - footer row of total sales/staff (including grand total for sales table) that updates when more stores are added
+ - a row with the data from each store, including stores added by the form
+
+*/
+
 
 //Sticky nav - https://www.w3schools.com/howto/howto_js_sticky_header.asp
 window.onscroll = function() {stickyNav();};
@@ -285,7 +66,6 @@ function stickyNav() {
 }
 
 //Slideshow - https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_auto
-
 let slideIndex = 0;
 showSlides();
 
@@ -302,16 +82,3 @@ function showSlides() {
   slides[slideIndex-1].style.display = 'block';
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
-
-
-// function addNewRow() {
-//   const table = document.getElementById('modelTable');
-
-//   let text = [storeLocation,minCust,maxCust,avgSale];
-//   let row = table.insertRow(table.rows.length-1);
-
-//   for (let i = 0; i < table.rows[0].cells.length; i++) {
-//     row.insertCell(-1).textContent = text[i] || '';
-//   }
-// }
-
